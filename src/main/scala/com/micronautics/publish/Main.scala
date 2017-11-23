741b94802dcd7bfd49b2409f1b069facd1d84478
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-22 16:39:17 -0800

    -

--- src/main/scala/com/micronautics/publish/Main.scala
+++ src/main/scala/com/micronautics/publish/Main.scala
@@ -6,16 +6,16 @@ import org.slf4j.Logger
 
 /** Utility for creating combined Scaladoc for an SBT multi-project */
 object Main extends App with OptionParsing {
+  implicit val commandLine: CommandLine = new CommandLine()
+
+  implicit val logger: Logger = org.slf4j.LoggerFactory.getLogger("pub")
+
   parser.parse(args, Config.default) match {
      case Some(config) => doIt(config)
 
      case None =>
        // arguments are bad, error message will have been displayed
    }
-
-  implicit val commandLine: CommandLine = new CommandLine
-
-  implicit val logger: Logger = org.slf4j.LoggerFactory.getLogger("pub")
 
   def doIt(implicit config: Config): Unit = {
     implicit val project: Project = Project(
